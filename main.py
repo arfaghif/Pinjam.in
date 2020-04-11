5562c27491db610d2d57188ddba86699ca60d9f8
falahsutawindaya <13518102@std.stei.itb.ac.id>
2020-04-11 19:28:49 +0700

    add search features and transaction

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-
 from command import * 
-# Setup UI
-# Login session (Akun yang login)
-
-
 
 # Password DB, dan Database pada Client mySql
-dB = DB("aaaaaaaaa","rpl")
+dB = DB("afterlife86","rpl")
 
 # Akun
 akun=Akun(dB)
-
 command = Command(akun)
 
-
-
-
+# Setup UI
 nav="navigasi dengan angka(lihat menu): "
 command.header()
 user = str(input(nav))
@@ -33,22 +25,48 @@ while not(user=='exit'):
   elif (user=='3'):
     # List semua kendaraan
     command.listKendaraan()
-    
     # Navigasi untuk memilih kendaraan
     user = str(input("ketik 'pilih kend []' untuk lebih lengkap: "))
     index=int(user[-1])-1
-    
     command.pilihKendaraan(index)
-    print("Tampilkan kontak penyewa? Y/N")
+    print("Tampilkan kontak penyewa? Y/N : Tampilkan kontak dan Lanjutkan Penawaran")
     user = input()
     if (user =="Y" or user == "y"):
       command.tampilkanKontak()
+    print("Ajukan Penawaran? Y/N")
+    user = input()
+    if (user =="Y" or user == "y"):
+      command.nominalPenawaran()
   elif (user=='4'):
     # Akun logout, Akun.loginSession menjadi null kembali
     akun.logout()
   elif (user=='5'):
     # REGISTER KENDARAAN, akan di validasi untuk mengarah ke halaman login
     command.regisKendaraan()
+  elif (user=='6'):
+    command.daftarTransaksi()
+  elif ('konfirmasi' in user):
+    command.konfirmasiPenawaran()
+    command.daftarTransaksi()
+  elif (user=='7'):
+    print("Anda yakin menjadi penyewa? Y/N")
+    user=input()
+    if (user =="Y" or user == "y"):
+      command.jadiPenyewa()
+  elif (user=='8'):
+    ada=command.cariKendaraan()
+    if (ada==True):
+      user = str(input("ketik 'pilih kend []' untuk lebih lengkap: "))
+      index=int(user[-1])-1
+      command.pilihKendaraan(index)
+      print("Tampilkan kontak penyewa? Y/N : Tampilkan kontak dan Lanjutkan Penawaran")
+      user = input()
+      if (user =="Y" or user == "y"):
+        command.tampilkanKontak()
+      print("Ajukan Penawaran? Y/N")
+      user = input()
+      if (user =="Y" or user == "y"):
+        command.nominalPenawaran()
   else :
     print("Navigasi angka tidak dikenal\n  ")
   
